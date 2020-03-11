7ec6bae497a4b99a4a9b238513e22b19263083f2
Ayush Agrawal <agrawal.ayush2500@gmail.com>
2020-03-11 12:53:24 +0530

    Listing Added

--- src/EventList/EventList.js
+++ src/EventList/EventList.js
@@ -42,6 +42,7 @@ class EventList extends Component{
             });
 
             let qbEventTitleWrapProps=[];
+
             for(let i=0; i<this.state.qbEventTitleWraps.length; i++)
             {
                 let qbEventTitleWrap=this.state.qbEventTitleWraps[i];
@@ -53,7 +54,9 @@ class EventList extends Component{
                     endPoint:{
                         x:qbEventTitleWrap.getBoundingClientRect().left+qbEventTitleWrap.getBoundingClientRect().width,
                         y:qbEventTitleWrap.getBoundingClientRect().top+qbEventTitleWrap.getBoundingClientRect().height,
-                    }
+                    },
+                    index:i,
+                    id:qbEventTitleWrap.id
                 }
 
                 qbEventTitleWrapProps.push(qbEventTitleWrapProp);
@@ -70,9 +73,7 @@ class EventList extends Component{
                 let qbEventTitle= this.state.qbEventTitles[i];
                 
                 let isMouseDown=false;
-                let qbEventTitleTargetId;
 
-                let qbEventTitleWrapProps=this.state.qbEventTitleWrapProps;
 
                 let qbEventTitleOffset={
                     x:null,
@@ -80,19 +81,28 @@ class EventList extends Component{
                 }
 
                 
+
                 let qbEventTitleNewOffset={
                     x:null,
                     y:null
                 }
 
+                let qbEventTitleIndex=0;
                 qbEventTitle.addEventListener("mousedown", function(event){
                     isMouseDown=true;
-                    qbEventTitleTargetId=event.target.id;
 
                     qbEventTitleOffset.x=0-event.clientX;
                     qbEventTitleOffset.y=0-event.clientY;
 
                     qbEventTitle.style.zIndex=15;
+
+                    for(let k=0; k<qbEventTitleWrapProps.length; k++)
+                    {
+                        if(qbEventTitleWrapProps[k].id==qbEventTitle.id)
+                        {
+                            qbEventTitleIndex=qbEventTitleWrapProps[k].index;
+                        }
+                    }
 
                 });
                 
@@ -119,25 +129,98 @@ class EventList extends Component{
                             }
 
                             if((qbEventTitleProp.startPoint.x>qbEventTitleWrapProps[j].startPoint.x)&&(qbEventTitleProp.startPoint.y>qbEventTitleWrapProps[j].startPoint.y)&&(qbEventTitleProp.endPoint.x<qbEventTitleWrapProps[j].endPoint.x)&&(qbEventTitleProp.endPoint.y<qbEventTitleWrapProps[j].endPoint.y))
-                            {
-                                if(i>j)
+                            {   
+                                 
+                                if(qbEventTitleIndex>j)
                                 {
-                                    console.log("Hellos");
-                                }
-                                else if(i<j)
-                                {
-                                    console.log(j);
-                                    
-                                    
-                                    qbEventTitle.style.left="unset";
-                                    qbEventTitle.style.top="unset";
-                                    for(let k=i+1; k<=j; k++)
+                                    if(j==0)
                                     {
-                                        qbEventTitleWraps[k-1].append(qbEventTitles[k]);
+                                        qbEventTitleWraps[0].parentElement.prepend(qbEventTitle.parentElement);
+                                        
+                                        qbEventTitleOffset.x=0-event.clientX;
+                                        qbEventTitleOffset.y=0-event.clientY;
+    
+                                        qbEventTitle.style.top="0px";
+                                    
+                                        let qbEventTitleNewIndex;
+                                        for(let k=0; k<qbEventTitleWrapProps.length; k++)
+                                        {
+                                            if(qbEventTitleWrapProps[k].id==qbEventTitleWraps[j].id)
+                                            {
+                                                qbEventTitleNewIndex=qbEventTitleWrapProps[k].index;
+                                                qbEventTitleWrapProps[k].index=qbEventTitleIndex;
+                                            }
+                                        }
+                                            
+                                        for(let k=0; k<qbEventTitleWrapProps.length; k++)
+                                        {
+                                            if(qbEventTitleWrapProps[k].id==qbEventTitle.id)
+                                            {
+                                                qbEventTitleWrapProps[k].index=qbEventTitleNewIndex;
+                                            }
+                                        }
                                     }
-                                    qbEventTitleWraps[j].append(qbEventTitle);
+                                    else
+                                    {
+                                        qbEventTitleWraps[0].parentElement.insertBefore(qbEventTitle.parentElement,qbEventTitleWraps[j]);          
+                                        
+                                        qbEventTitleOffset.x=0-event.clientX;
+                                        qbEventTitleOffset.y=0-event.clientY;
+    
+                                        qbEventTitle.style.top="0px";
+                                    
+                                        let qbEventTitleNewIndex;
+                                        for(let k=0; k<qbEventTitleWrapProps.length; k++)
+                                        {
+                                            if(qbEventTitleWrapProps[k].id==qbEventTitleWraps[j].id)
+                                            {
+                                                qbEventTitleNewIndex=qbEventTitleWrapProps[k].index;
+                                                qbEventTitleWrapProps[k].index=qbEventTitleIndex;
+                                            }
+                                        }
+                                            
+                                        for(let k=0; k<qbEventTitleWrapProps.length; k++)
+                                        {
+                                            if(qbEventTitleWrapProps[k].id==qbEventTitle.id)
+                                            {
+                                                qbEventTitleWrapProps[k].index=qbEventTitleNewIndex;
+                                            }
+                                        }
+                                    }
+                                }
+                                else if(qbEventTitleIndex<j)
+                                {
+                                    if(j==qbEventTitles.length)
+                                    {
+                                        qbEventTitleWraps[0].parentElement.append(qbEventTitle.parentElement);
+                                    }
+                                    else
+                                    {
+                                        qbEventTitleWraps[0].parentElement.insertBefore(qbEventTitle.parentElement,qbEventTitleWraps[j+1]);          
+                                    }
+                                    let qbEventTitleNewIndex;
+                                    for(let k=0; k<qbEventTitleWrapProps.length; k++)
+                                    {
+                                        if(qbEventTitleWrapProps[k].id==qbEventTitleWraps[j].id)
+                                        {
+                                            qbEventTitleNewIndex=qbEventTitleWrapProps[k].index;
+                                            qbEventTitleWrapProps[k].index=qbEventTitleIndex;
+                                        }
+                                    }
+                                        
+                                    for(let k=0; k<qbEventTitleWrapProps.length; k++)
+                                    {
+                                        if(qbEventTitleWrapProps[k].id==qbEventTitle.id)
+                                        {
+                                            qbEventTitleWrapProps[k].index=qbEventTitleNewIndex;
+                                        }
+                                    }
                                     
                                     
+                                    qbEventTitleOffset.x=0-event.clientX;
+                                    qbEventTitleOffset.y=0-event.clientY;
+                                    qbEventTitle.style.top="0px";
+                                
                                 }
                             }
                         }
@@ -153,36 +236,8 @@ class EventList extends Component{
 
                     for(let j=0; j<qbEventTitleWrapProps.length; j++)
                     {
-                        let qbEventTitleProp={
-                            startPoint:{
-                                x:qbEventTitle.getBoundingClientRect().x,
-                                y:qbEventTitle.getBoundingClientRect().y
-                            },
-                            endPoint:{
-                                x:qbEventTitle.getBoundingClientRect().x+qbEventTitle.getBoundingClientRect().width,
-                                y:qbEventTitle.getBoundingClientRect().y+qbEventTitle.getBoundingClientRect().height
-                            }
-                        }
-
-                        if((qbEventTitleProp.startPoint.x>qbEventTitleWrapProps[j].startPoint.x)&&(qbEventTitleProp.startPoint.y>qbEventTitleWrapProps[j].startPoint.y)&&(qbEventTitleProp.endPoint.x<qbEventTitleWrapProps[j].endPoint.x)&&(qbEventTitleProp.endPoint.y<qbEventTitleWrapProps[j].endPoint.y))
-                        {
-                            /*
-                            if(i>j)
-                            {
-                                console.log("Hellos");
-                                qbEventTitleWraps[j].append(qbEventTitle)
-                                qbEventTitle.style.left="unset";
-                                qbEventTitle.style.top="unset";
-                            }
-                            else if(i<j)
-                            {
-                                console.log("Hellos 1");
-                                qbEventTitleWraps[j].append(qbEventTitle)
-                                qbEventTitle.style.left="unset";
-                                qbEventTitle.style.top="unset";
-                            }
-                             */
-                        }
+                        qbEventTitles[j].style.top="0px";
+                        qbEventTitles[j].style.left="0px";
                     }
 
                     qbEventTitleOffset.x=0;
