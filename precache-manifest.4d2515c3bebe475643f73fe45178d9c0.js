5117c9127ea4c64a77bf73fec3bec16bdd48023b
Ayush Agrawal <agrawal.ayush2500@gmail.com>
2020-03-12 16:30:17 +0530

    Updates

---
+++ precache-manifest.4d2515c3bebe475643f73fe45178d9c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a12d9419e217c5e8437844f3b4d805f5",
+    "url": "/EventListTwo/index.html"
+  },
+  {
+    "revision": "6d37c8798bf2c893539c",
+    "url": "/EventListTwo/static/css/main.04eeef33.chunk.css"
+  },
+  {
+    "revision": "79d86a5d3f11c10e3bcb",
+    "url": "/EventListTwo/static/js/2.2af7f28d.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/EventListTwo/static/js/2.2af7f28d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d37c8798bf2c893539c",
+    "url": "/EventListTwo/static/js/main.2db2becf.chunk.js"
+  },
+  {
+    "revision": "c4487342feb285a1f9d9",
+    "url": "/EventListTwo/static/js/runtime-main.d07d7b2a.js"
+  }
+]);
